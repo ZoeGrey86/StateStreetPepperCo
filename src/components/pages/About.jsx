1227c3a489eac3d8335a107a607a5f92486640bb
Zoe Smith <107585629+ZoeGrey86@users.noreply.github.com>
2023-10-26 10:37:22 -0500

    pages are ready for better styling

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,25 +1,47 @@
 import React from "react";
-import "./pages.css";
+import "./AboutUs.css";
+import joe from './JoeTimm.jpg';
+import micheal from './MichealHoltz.jpg';
+import layton from './LaytonDiaz.jpg';
+import zoe from './ZoeGrey.jpg';
 
 export const About = () => {
-    return(
-        <div>
-            <h1>About Us!</h1>
-            <div>
-                <h1>Joe Timm: The Master of Dad Jokes 😂</h1>
-                <p> Part-time stand-up comedian, full-time dad-joke connoisseur. If laughter is the best medicine, then I'm your prescription. Remember, I'm not just funny; I'm "dad-joke-at-a-wedding" funny.</p>
+    return (
+        <div class="about-us-container">
+            <div class="profile">
+                <div class="profile-picture">
+                    <img src={joe} alt="Joe Timm" />
+                </div>
+                <div class="profile-name">Joe Timm</div>
+                <div class="profile-title">The Master of Dad Jokes 😂</div>
+                <p class="profile-bio">Part-time stand-up comedian, full-time dad-joke connoisseur. If laughter is the best medicine, then I'm your prescription. Remember, I'm not just funny; I'm "dad-joke-at-a-wedding" funny.</p>
             </div>
-            <div>
-                <h1>Michael Holtz: Professional procrastinator </h1>
-                <p> The man who once tried to teach a goldfish to fetch. Spoiler alert: The goldfish won. 🐟 #FishWhisperer" If I'm not late, I'm not trying hard enough.</p>
+
+            <div class="profile">
+                <div class="profile-picture">
+                    <img src={micheal} alt="Micheal Holtz" />
+                </div>
+                <div class="profile-name">Micheal Holtz</div>
+                <div class="profile-title">Professional procrastinator</div>
+                <p class="profile-bio"> The man who once tried to teach a goldfish to fetch. Spoiler alert: The goldfish won. 🐟 #FishWhisperer" If I'm not late, I'm not trying hard enough.</p>
             </div>
-            <div>
-                <h1>Layton Diaz: The Dancing Taco Enthusiast 💃🌮</h1>
-                <p> Layton Diaz - Salsa on the dance floor, salsa on the taco – Layton's got the moves for both! 💃🕺 I cook when I have to, and I eat out when I don't. On a mission to find the world's best taco.</p>
+
+            <div class="profile">
+                <div class="profile-picture">
+                    <img src={layton} alt="Layton Diaz" />
+                </div>
+                <div class="profile-name">Layton Diaz</div>
+                <div class="profile-title">The Dancing Taco Enthusiast 💃🌮</div>
+                <p class="profile-bio">Salsa on the dance floor, salsa on the taco – Layton's got the moves for both! 💃🕺 I cook when I have to, and I eat out when I don't. On a mission to find the world's best taco.</p>
             </div>
-            <div>
-                <h1>Zoe Grey: The Coffee Wizard ☕✨</h1>
-                <p>Zoe Grey - She turns caffeine into code, one espresso shot at a time. A true sorceress of the tech world. 🧙‍♀️💻 I run on coffee, chaos, and the belief that one day, I'll be a unicorn. 🦄✨</p>
+
+            <div class="profile">
+                <div class="profile-picture">
+                    <img src={zoe} alt="Zoe Grey" />
+                </div>
+                <div class="profile-name">Zoe Grey</div>
+                <div class="profile-title"> The Coffee Wizard ☕✨</div>
+                <p class="profile-bio">She turns caffeine into code, one espresso shot at a time. A true sorceress of the tech world. 🧙‍♀️💻 I run on coffee, chaos, and the belief that one day, I'll be a unicorn. 🦄✨</p>
             </div>
         </div>
     )
--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -1,141 +1,141 @@
 import React from "react";
-import "./pages.css";
+import "./Products.css";
+import zoe from './ZoeGrey.jpg';
 
-export const Products = () =>{
-    return(
-        <div>
-           <h1>Check Out Our Goods</h1>
-      <div >
-        <img alt='hot sauce bottle'></img>
-        <h1 >Zoe's Butt Bittin Buffalo Sauce</h1>
-        <p>
-          Introducing Zoe's Butt Bittin Buffalo Sauce, the fiery elixir that'll
-          set your taste buds ablaze with flavor! Crafted by the one and only
-          Zoe Grey, our resident flavor sorceress, this sauce is like a flavor
-          explosion in your mouth. With a tantalizing blend of zesty cayenne
-          peppers, secret spices, and a dash of Zoe's signature wit, Butt Bittin
-          Buffalo Sauce delivers a punch of heat that's balanced with a symphony
-          of mouthwatering flavors. It's so addictive; you'll find yourself
-          pouring it on everything from crispy chicken wings to veggies and even
-          your morning eggs. But beware, this sauce doesn't just bite; it's a
-          full-blown flavor assault! Zoe's Butt Bittin Buffalo Sauce isn't for
-          the faint of heart, but for those who dare to dance with the fiery
-          dragon, it's a journey you won't soon forget. So, get ready to have
-          your taste buds tingling and your senses sizzling with each delectable
-          drop of Zoe's Butt Bittin Buffalo Sauce – it's flavor with a fierce
-          kick!
-        </p>
-        <p>price: $MONEY</p>
-      </div>
-      <div >
-        <img alt='hot sauce bottle'></img>
-        <h1 >Blueberry Babe Hot Sauce</h1>
-        <p>
-          Introducing our delectable Blueberry Babe Hot Sauce, where the sweet
-          meets the heat in a mouthwatering fusion of flavors! Crafted with care
-          and a dash of culinary creativity, this sauce is your passport to a
-          flavor adventure like no other. Picture plump, juicy blueberries
-          handpicked at the peak of ripeness, combined with the fiery embrace of
-          carefully selected chili peppers. The result? A tantalizing symphony
-          of taste that will tingle your taste buds with every bite. Blueberry
-          Babe Hot Sauce isn't just a condiment; it's a culinary masterpiece.
-          It's the perfect balance of sweet and spicy, making it a versatile
-          addition to your kitchen repertoire. Drizzle it over grilled meats for
-          an unforgettable BBQ experience, swirl it into your morning yogurt to
-          wake up your senses, or even use it as a dipping sauce for a
-          surprising twist on your favorite snacks. Get ready to embark on a
-          flavor journey that will leave you craving more. Our Blueberry Babe
-          Hot Sauce is not just a sauce; it's an invitation to explore the bold,
-          the sweet, and the deliciously spicy in every dish. So, take your
-          taste buds on an adventure they won't forget with a bottle of
-          Blueberry Babe Hot Sauce – where sweetness and heat collide in the
-          most delightful way!
-        </p>
-        <p>price: $MONEY</p>
-      </div>
-      <div >
-        <img alt='hot sauce bottle'></img>
-        <h1 >Garlic Inferno Hot Sauce</h1>
-        <p>
-          Introducing Garlic Inferno Hot Sauce, where the bold embrace of fiery
-          heat and the robust allure of garlic come together to ignite your
-          taste buds in a sizzling symphony of flavor! Brace yourself for an
-          inferno of culinary excitement that will leave you craving more. Our
-          Garlic Inferno Hot Sauce is not for the faint of heart. It's a thick,
-          velvety elixir that packs a punch with every drop. Picture plump,
-          roasted garlic cloves mingling with a carefully curated blend of
-          scorching chili peppers, creating a sensation that's nothing short of
-          addictive. This sauce isn't just about heat; it's a full-on flavor
-          revolution. Slather it on your grilled steaks for an instant flavor
-          upgrade, swirl it into your pasta for a bold Italian twist, or use it
-          as a dipping sauce to turn everyday snacks into fiery sensations. But
-          beware, this isn't your ordinary hot sauce; it's a Garlic Inferno
-          waiting to engulf your taste buds in a blaze of savory spice. Get
-          ready to elevate your culinary creations to a whole new level with
-          every dollop, drizzle, or dunk of Garlic Inferno Hot Sauce. It's not
-          just hot; it's a garlic lover's fiery dream come true!
-        </p>
-        <p>price: $MONEY</p>
-      </div>
-      <div >
-        <img alt='hot sauce bottle'></img>
-        <h1 >7 Pepper Mango Hot Sauce</h1>
-        <p>
-          Introducing our 7 Pepper Mango Hot Sauce, a tantalizing fusion of
-          sweet and heat that will transport your taste buds on a flavorful
-          journey like no other. Crafted with precision and passion, this sauce
-          is a celebration of seven unique peppers dancing in harmony with the
-          tropical sweetness of ripe mangoes. Picture a symphony of seven
-          distinct peppers, each with its own fiery personality, coming together
-          to create a crescendo of flavor. From smoky chipotle to fiery
-          habanero, this hot sauce boasts a complex and unforgettable heat
-          profile that will keep you coming back for more. But it's not just
-          about the heat; it's also about the tropical bliss of ripe mangoes.
-          The sweet and fruity notes of mango perfectly balance the fiery
-          peppers, creating a sauce that's not only spicy but also delightfully
-          flavorful. Drizzle it over grilled shrimp for a taste of the
-          Caribbean, mix it into your favorite marinades for an exotic twist, or
-          simply use it as a dipping sauce to elevate your snacks to a whole new
-          level. With our 7 Pepper Mango Hot Sauce, you're not just adding heat
-          to your dishes; you're infusing them with a symphony of flavors that
-          will awaken your taste buds and leave them craving more. Get ready to
-          embark on a culinary adventure where sweetness and spice coalesce in a
-          harmonious explosion of taste. It's not just a hot sauce; it's a
-          tropical paradise in a bottle!
-        </p>
-        <p>price: $MONEY</p>
-      </div>
-      <div >
-        <img alt='hot sauce bottle'></img>
-        <h1 >Danger Corn Hot Sauce</h1>
-        <p>
-          Introducing Danger Corn Hot Sauce, a fiery elixir that's not for the
-          faint of heart, made with the electrifying Cumari peppers. This sauce
-          is a thrilling adventure for those who crave heat with a side of
-          daring flavor. Picture the Cumari peppers, tiny but mighty,
-          hand-selected for their intense spice and unique flavor profile. These
-          little powerhouses are the stars of our show, bringing an
-          unforgettable heat that will make your taste buds stand at attention.
-          Danger Corn Hot Sauce is more than just a condiment; it's a
-          daredevil's dream. With the unmistakable bite of the Cumari peppers
-          and a hint of smokiness, this sauce will have you redefining your
-          spice tolerance. It's perfect for those who live life on the edge of
-          their taste buds, seeking bold and thrilling culinary experiences.
-          Whether you're drizzling it over your favorite grilled corn on the cob
-          or daring to dip your snacks into its fiery embrace, Danger Corn Hot
-          Sauce promises to ignite your palate and leave you craving the
-          electrifying heat of Cumari peppers. It's not just hot sauce; it's a
-          journey to the edge of flavor, where danger meets deliciousness!
-        </p>
-        <p>price: $MONEY</p>
-      </div>
-      <div >
-        <img alt='hot sauce bottle'></img>
-        <h1 > Hot Sauce</h1>
-        <p>about</p>
-        <p>price: $MONEY</p>
-      </div>
+export const Products = () => {
+  return (
+    <body>
+      <div className="product-container">
+        <div className="products">
+          <img src={zoe} alt="hot sauce" />
+          <h3>Zoe's Butt Bittin Buffalo Sauce</h3>
+          <p className="description">
+            Introducing Zoe's Butt Bittin Buffalo Sauce, the fiery elixir that'll
+            set your taste buds ablaze with flavor! Crafted by the one and only
+            Zoe Grey, our resident flavor sorceress, this sauce is like a flavor
+            explosion in your mouth. With a tantalizing blend of zesty cayenne
+            peppers, secret spices, and a dash of Zoe's signature wit, Butt Bittin
+            Buffalo Sauce delivers a punch of heat that's balanced with a symphony
+            of mouthwatering flavors. It's so addictive; you'll find yourself
+            pouring it on everything from crispy chicken wings to veggies and even
+            your morning eggs. But beware, this sauce doesn't just bite; it's a
+            full-blown flavor assault! Zoe's Butt Bittin Buffalo Sauce isn't for
+            the faint of heart, but for those who dare to dance with the fiery
+            dragon, it's a journey you won't soon forget. So, get ready to have
+            your taste buds tingling and your senses sizzling with each delectable
+            drop of Zoe's Butt Bittin Buffalo Sauce – it's flavor with a fierce
+            kick!
+
+          </p>
+          <p className="price">$15</p>
         </div>
-    )
+        <div className="products">
+          <img src={zoe} alt="hot sauce" />
+          <h3>Blueberry Babe Hot Sauce</h3>
+          <p className="description">
+            Introducing our delectable Blueberry Babe Hot Sauce, where the sweet
+            meets the heat in a mouthwatering fusion of flavors! Crafted with care
+            and a dash of culinary creativity, this sauce is your passport to a
+            flavor adventure like no other. Picture plump, juicy blueberries
+            handpicked at the peak of ripeness, combined with the fiery embrace of
+            carefully selected chili peppers. The result? A tantalizing symphony
+            of taste that will tingle your taste buds with every bite. Blueberry
+            Babe Hot Sauce isn't just a condiment; it's a culinary masterpiece.
+            It's the perfect balance of sweet and spicy, making it a versatile
+            addition to your kitchen repertoire. Drizzle it over grilled meats for
+            an unforgettable BBQ experience, swirl it into your morning yogurt to
+            wake up your senses, or even use it as a dipping sauce for a
+            surprising twist on your favorite snacks. Get ready to embark on a
+            flavor journey that will leave you craving more. Our Blueberry Babe
+            Hot Sauce is not just a sauce; it's an invitation to explore the bold,
+            the sweet, and the deliciously spicy in every dish. So, take your
+            taste buds on an adventure they won't forget with a bottle of
+            Blueberry Babe Hot Sauce – where sweetness and heat collide in the
+            most delightful way!
+          </p>
+          <p className="price">$15</p>
+        </div>
+        <div className="products">
+          <img src={zoe} alt="hot sauce" />
+          <h3>Garlic Inferno Hot Sauce</h3>
+          <p className="description">
+            Introducing Garlic Inferno Hot Sauce, where the bold embrace of fiery
+            heat and the robust allure of garlic come together to ignite your
+            taste buds in a sizzling symphony of flavor! Brace yourself for an
+            inferno of culinary excitement that will leave you craving more. Our
+            Garlic Inferno Hot Sauce is not for the faint of heart. It's a thick,
+            velvety elixir that packs a punch with every drop. Picture plump,
+            roasted garlic cloves mingling with a carefully curated blend of
+            scorching chili peppers, creating a sensation that's nothing short of
+            addictive. This sauce isn't just about heat; it's a full-on flavor
+            revolution. Slather it on your grilled steaks for an instant flavor
+            upgrade, swirl it into your pasta for a bold Italian twist, or use it
+            as a dipping sauce to turn everyday snacks into fiery sensations. But
+            beware, this isn't your ordinary hot sauce; it's a Garlic Inferno
+            waiting to engulf your taste buds in a blaze of savory spice. Get
+            ready to elevate your culinary creations to a whole new level with
+            every dollop, drizzle, or dunk of Garlic Inferno Hot Sauce. It's not
+            just hot; it's a garlic lover's fiery dream come true!
 
-}
+          </p>
+          <p className="price">$15</p>
+        </div>
+        <div className="products">
+          <img src={zoe} alt="hot sauce" />
+          <h3>7 Pepper Mango Hot Sauce</h3>
+          <p className="description">
+            Introducing our 7 Pepper Mango Hot Sauce, a tantalizing fusion of
+            sweet and heat that will transport your taste buds on a flavorful
+            journey like no other. Crafted with precision and passion, this sauce
+            is a celebration of seven unique peppers dancing in harmony with the
+            tropical sweetness of ripe mangoes. Picture a symphony of seven
+            distinct peppers, each with its own fiery personality, coming together
+            to create a crescendo of flavor. From smoky chipotle to fiery
+            habanero, this hot sauce boasts a complex and unforgettable heat
+            profile that will keep you coming back for more. But it's not just
+            about the heat; it's also about the tropical bliss of ripe mangoes.
+            The sweet and fruity notes of mango perfectly balance the fiery
+            peppers, creating a sauce that's not only spicy but also delightfully
+            flavorful. Drizzle it over grilled shrimp for a taste of the
+            Caribbean, mix it into your favorite marinades for an exotic twist, or
+            simply use it as a dipping sauce to elevate your snacks to a whole new
+            level. With our 7 Pepper Mango Hot Sauce, you're not just adding heat
+            to your dishes; you're infusing them with a symphony of flavors that
+            will awaken your taste buds and leave them craving more. Get ready to
+            embark on a culinary adventure where sweetness and spice coalesce in a
+            harmonious explosion of taste. It's not just a hot sauce; it's a
+            tropical paradise in a bottle!
+
+          </p>
+          <p className="price">$15</p>
+        </div>
+        <div className="products">
+          <img src={zoe} alt="hot sauce" />
+          <h3>Danger Corn Hot Sauce</h3>
+          <p className="description">
+            Introducing Danger Corn Hot Sauce, a fiery elixir that's not for the
+            faint of heart, made with the electrifying Cumari peppers. This sauce
+            is a thrilling adventure for those who crave heat with a side of
+            daring flavor. Picture the Cumari peppers, tiny but mighty,
+            hand-selected for their intense spice and unique flavor profile. These
+            little powerhouses are the stars of our show, bringing an
+            unforgettable heat that will make your taste buds stand at attention.
+            Danger Corn Hot Sauce is more than just a condiment; it's a
+            daredevil's dream. With the unmistakable bite of the Cumari peppers
+            and a hint of smokiness, this sauce will have you redefining your
+            spice tolerance. It's perfect for those who live life on the edge of
+            their taste buds, seeking bold and thrilling culinary experiences.
+            Whether you're drizzling it over your favorite grilled corn on the cob
+            or daring to dip your snacks into its fiery embrace, Danger Corn Hot
+            Sauce promises to ignite your palate and leave you craving the
+            electrifying heat of Cumari peppers. It's not just hot sauce; it's a
+            journey to the edge of flavor, where danger meets deliciousness!
+
+          </p>
+          <p className="price">$15</p>
+        </div>
+      </div>
+    </body>
+  )
+
+};
